c74fcdd67835b6b131dcecb574a8bb4c40132a8f
Salman-Mesam <59708296+Salman-Mesam@users.noreply.github.com>
2022-06-27 16:18:56 +0000

    Initial commit

---
+++ src/test/java/net/javaguides/springboot/SpringbootBackendApplicationTests.java
@@ -0,0 +1,15 @@
+package net.javaguides.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+//salman mesam
+@SpringBootTest
+class SpringbootBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
